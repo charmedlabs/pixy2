2f5512d75a24a62963eda8c5b52d9a0ec6331d54
Explorer <null@null.net>
2018-06-15 18:17:56 -0500

    Relocate get_blocks_cpp_demo

---
+++ src/host/get_blocks_cpp_demo/get_blocks_cpp_demo.cpp
@@ -0,0 +1,36 @@
+#include "libpixyusb2.h"
+
+Pixy2 pixy;
+
+void loop()
+{
+
+  int i;
+  // grab blocks!
+  pixy.ccc.getBlocks();
+
+  // If there are detect blocks, print them!
+  if (pixy.ccc.numBlocks)
+  {
+    printf("Detected %d\n", pixy.ccc.numBlocks);
+    for (i=0; i<pixy.ccc.numBlocks; i++)
+    {
+      printf("  block %d: ", i);
+      pixy.ccc.blocks[i].print();
+    }
+  }
+  pixy.m_link.callChirp("hello");
+}
+
+int main()
+{
+  int res;
+  res = pixy.init();
+  if (res<0)
+  {
+    printf("unable to open pixy object\n");
+    return 0;
+  }
+  while(1)
+    loop();
+}
