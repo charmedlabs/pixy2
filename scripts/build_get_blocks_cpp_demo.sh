1a36a4cf26ad74a0db85fa9f751774927cb1eeca
Explorer <null@null.net>
2018-06-15 18:17:56 -0500

    Relocate get_blocks_cpp_demo

---
+++ scripts/build_get_blocks_cpp_demo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Starting build..."
+
+set -x
+
+TARGET_BUILD_FOLDER=../build
+
+mkdir $TARGET_BUILD_FOLDER
+mkdir $TARGET_BUILD_FOLDER/get_blocks_cpp_demo
+
+echo "Starting build..."
+cd ../src/host/get_blocks_cpp_demo
+make
+mv ./get_blocks_cpp_demo ../../../build/get_blocks_cpp_demo
