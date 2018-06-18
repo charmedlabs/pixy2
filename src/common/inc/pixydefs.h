401d7a8a5ba138f184835a0a83c583b325e09c0c
Explorer <beachesandtrees532@protonmail.com>
2018-06-17 19:05:46 -0500

    Add new definitions.

--- src/common/inc/pixydefs.h
+++ src/common/inc/pixydefs.h
@@ -35,6 +35,27 @@
 #define PIXY_TYPE_REQUEST_SERVO              0x12
 #define PIXY_TYPE_REQUEST_LED                0x14
 
+#define CCC_MAX_SIGNATURE                    7
+
+#define CCC_RESPONSE_BLOCKS                  0x21
+#define CCC_REQUEST_BLOCKS                   0x20
+
+// Defines for sigmap:
+// You can bitwise "or" these together to make a custom sigmap.
+// For example if you're only interested in receiving blocks
+// with signatures 1 and 5, you could use a sigmap of
+// PIXY_SIG1 | PIXY_SIG5
+#define CCC_SIG1                             1
+#define CCC_SIG2                             2
+#define CCC_SIG3                             4
+#define CCC_SIG4                             8
+#define CCC_SIG5                             16
+#define CCC_SIG6                             32
+#define CCC_SIG7                             64
+#define CCC_COLOR_CODES                      128
+
+#define CCC_SIG_ALL                          0xff // all bits or'ed together
+
 #define PIXY_SIG1                            1
 #define PIXY_SIG2                            2
 #define PIXY_SIG3                            4
