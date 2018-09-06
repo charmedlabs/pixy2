c2027e63e8ebf505f423ffbdd1636e7bb1810c47
rich legrand <rich@charmedlabs.com>
2018-09-06 16:25:00 -0500

    ...

--- src/device/main_m4/inc/exec.h
+++ src/device/main_m4/inc/exec.h
@@ -25,7 +25,7 @@
 
 #define FW_MAJOR_VER		      3
 #define FW_MINOR_VER		      0
-#define FW_BUILD_VER		      12
+#define FW_BUILD_VER		      11
 #ifdef LEGO 
 #define FW_TYPE                   "LEGO"
 #else
